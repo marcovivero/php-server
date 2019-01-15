90be8f20d66b56ccd3256b2d95b54432983ae856
marcovivero <mvivero091@gmail.com>
2019-01-15 15:54:44 -0800

    Initial nginx server

---
+++ docker/bin/run-nginx.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+### Helper function for running nginx with security layered.
+
+apk add openssl
+
+mkdir /etc/nginx/certs/
+
+cd /etc/nginx/certs/
+openssl req -x509 -nodes -days 1024 -newkey rsa:2048 -keyout localhost.key -out localhost.crt \
+    -subj "/C=US/ST=California/L=Burlingame/O=Web Server/OU=Web Server/CN=localhost"
+
+envsubst < /etc/nginx/conf.d/nginx_custom.conf > /etc/nginx/conf.d/default.conf && \
+    nginx -g 'daemon off;'
